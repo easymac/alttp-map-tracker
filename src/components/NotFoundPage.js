c6856449d0b69e0199e842458028d04f51668e36
Mac Carrithers <mac@carrithers.me>
2017-07-10 16:48:21 -0600

    Add Sahasrahla's nicknames to the 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -4,11 +4,26 @@ import Image from './Image';
 import { Link } from 'react-router';
 
 const NotFoundPage = () => {
+  const sahanames = [
+    'sahasralah', 'sabotaging', 'sacahuista', 'sacahuiste', 'saccharase', 'saccharide', 'saccharify',
+    'saccharine', 'saccharins', 'sacerdotal', 'sackcloths', 'salmonella', 'saltarelli', 'saltarello',
+    'saltations', 'saltbushes', 'saltcellar', 'saltshaker', 'salubrious', 'sandgrouse', 'sandlotter',
+    'sandstorms', 'sandwiched', 'sauerkraut', 'schipperke', 'schismatic', 'schizocarp', 'schmalzier',
+    'schmeering', 'schmoosing', 'shibboleth', 'shovelnose', 'sahananana', 'sarararara', 'salamander',
+    'sharshalah', 'shahabadoo', 'sassafrass'
+  ];
+
+  // Pick a random sahaname, capitalize, and add 's or '
+  let sahaString = sahanames[Math.floor(Math.random() * (sahanames.length))];
+  sahaString = sahaString.charAt(0).toUpperCase() + sahaString.slice(1);
+  sahaString += (sahaString.charAt(sahaString.length - 1) == 's') ? "'" : "'s";
+
+
   const locations = [
-    'Sahasrahla\'s pocket',
-    'sick kid\'s bed',
+    `${sahaString} pocket`,
+    'Sick Kid\'s bed',
     'Hera basement',
-    'king\'s tomb'
+    'King\'s Tomb'
   ];
 
   const location = locations[Math.floor(Math.random() * (locations.length))];
