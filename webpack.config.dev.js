f75e891e7aab9bfe6e702d8254543103d60e064f
Mac Carrithers <mac@carrithers.me>
2017-07-25 20:57:45 -0600

    Add async polyfill

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,6 +13,7 @@ export default {
     './src/webpack-public-path',
     'react-hot-loader/patch',
     'webpack-hot-middleware/client?reload=true',
+    'babel-polyfill',
     path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
   ],
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
